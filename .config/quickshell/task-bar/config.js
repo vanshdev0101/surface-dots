31c800128dd96382fd336871db43d3a1708dd409
Vanshdev0101 <vanshdev0101@gmail.com>
2026-08-10 13:14:14 +0530

    Change Hub profile name from snes to Vansh (both layouts)

--- .config/quickshell/task-bar/config.js
+++ .config/quickshell/task-bar/config.js
@@ -2,7 +2,7 @@
 
 // hardcoding just for now.
 var PROFILE_IMG = "/home/vanshc/.config/quickshell/task-bar/profile.jpg"
-var PROFILE_NAME = "snes"
+var PROFILE_NAME = "Vansh"
 
 var TOP_GAP = 50
 var RIGHT_GAP = 10
